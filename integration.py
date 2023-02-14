835ee404225fd5c09139e92c686460234bb2680f
Michael <mayodemichaeloladimeji@gmail.com>
2023-02-14 21:24:44 +0100

    making call to first star ship

--- integration.py
+++ integration.py
@@ -5,5 +5,8 @@ BASE_URL = 'https://swapi.dev/api'
 all_people_response = requests.get(url=f'{BASE_URL}/people/').json()
 print(all_people_response)
 
-star_ships_schema = requests.get(url=f'{BASE_URL}/starships').json()
+star_ships_schema = requests.get(url=f'{BASE_URL}/starships/schema').json()
 print(star_ships_schema)
+
+first_star_ship = requests.get(url=f'{BASE_URL}/starships/1').json()
+print(first_star_ship)
