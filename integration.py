4adcaf06e7d839771310907a88c623dab5fac3fe
Michael <mayodemichaeloladimeji@gmail.com>
2023-02-14 21:18:29 +0100

    added starships call feature

--- integration.py
+++ integration.py
@@ -4,3 +4,6 @@ BASE_URL = 'https://swapi.dev/api'
 
 all_people_response = requests.get(url=f'{BASE_URL}/people/').json()
 print(all_people_response)
+
+star_ships_schema = requests.get(url=f'{BASE_URL}/starships').json()
+print(star_ships_schema)
